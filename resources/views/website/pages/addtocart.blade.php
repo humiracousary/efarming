47eb10cf4492bbcc809f5043da9dde6d7422fe9e
ah-rasel <18103234@iubat.edu>
2022-01-19 01:52:28 +0600

    Cart fixed

--- resources/views/website/pages/addtocart.blade.php
+++ resources/views/website/pages/addtocart.blade.php
@@ -2,82 +2,56 @@
 
 @section('content')
 
-{{-- @php 
-$carts = session('cart');
-$total = 0;
-@endphp
-
-
-@foreach($carts as $cart)
-<ul>
-  <li>name:{{$cart['name']}}
-    
-  </li>
-  <li>price:{{$cart['price']}}</li>
-</ul>
-
-@php 
-$total = $total+$cart['price'];
-@endphp
-
-@endforeach
-
-<ul>
-  <li>ttal:{{$total}}</li>
-</ul> --}}
-    
-
-@php
-$carts= session('cart');
-$total=0;
-@endphp
 
 <div class="container">
-<div class="row">
+  <div class="row">
     <div class="col-md-4"></div>
     <div class="col-md-4"></div>
     <div class="col-md-4"></div>
 
-<table class="table">
-    
-
-<thead>
-    <tr>
-        <th scope="col">#</th>
-        {{-- <th scope="col">Image</th> --}}
-        <th scope="col">Name</th>
-        <th scope="col">price</th>
-        <th scope="col">quantity</th>
-        <th scope="col">SubTotal</th>
-    </tr>
-</thead>
-
-@foreach($carts as $key=>$cart)
-
-<tr>
-    <td>{{$key+1}}</td>
-    {{-- <td>
-        <img width="100px" src="{{url('uploads/product/'.$cart['image'])}}" alt="">
-      </td> --}}
-    <td>{{$cart['name']}}</td>
-    <td>{{$cart['price']}}</td>
-    <td>{{$cart['quantity']}}</td>
-    <td>{{(int)$cart['quantity'] * (int)$cart['price']}}</td>
-</tr>
-
-@php
-$total=(int)$total+((int)$cart['quantity'] * (int)$cart['price']);
-@endphp
-@endforeach
+    <table class="table">
 
 
-</table>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          {{-- <th scope="col">Image</th> --}}
+          <th scope="col">Name</th>
+          <th scope="col">price</th>
+          <th scope="col">quantity</th>
+          <th scope="col">SubTotal</th>
+        </tr>
+      </thead>
+      @if ($carts)
+      @foreach($carts as $key=>$cart)
 
-<div style="float:right">
-    <p>Total={{$total}}</p>
-</div>
+      <tr>
+        <td>{{$key+1}}</td>
+        {{-- <td>
+          <img width="100px" src="{{url('uploads/product/'.$cart['image'])}}" alt="">
+        </td> --}}
+        <td>{{$cart['name']}}</td>
+        <td>{{$cart['price']}}</td>
+        <td>{{$cart['quantity']}}</td>
+        <td>{{(int)$cart['quantity'] * (int)$cart['price']}}</td>
+      </tr>
 
-</div>
+      @php
+      $total=(int)$total+((int)$cart['quantity'] * (int)$cart['price']);
+      @endphp
+      @endforeach
+      @endif
 
-@endsection
+    </table>
 
+    <div style="float:right">
+    @if ($carts)
+    <p>Total={{ $total }}</p>
+    @else
+    <p>There is no Products On The cart</p>
+    @endif
+    </div>
+
+  </div>
+
+  @endsection
