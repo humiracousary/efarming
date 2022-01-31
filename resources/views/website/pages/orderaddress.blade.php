5f150ff801bba4c64c2f9ac1018f27846108f6d6
humiracousary <18103310@iubat.edu>
2022-01-31 17:50:11 +0600

    oa

--- resources/views/website/pages/orderaddress.blade.php
+++ resources/views/website/pages/orderaddress.blade.php
@@ -11,19 +11,19 @@
         @csrf
         <div class="checkout__input">
         <label for="exampleInputPassword1">Receiver_Name</label>
-        <input value={{auth()->user()->name}} type="text" class="form-control" id="exampleInputPassword1" placeholder="name" name="name">
+        <input value="{{auth()->user()->name}}" type="text" class="form-control" id="exampleInputPassword1" placeholder="name" name="name">
         </div>
         <div class="checkout__input">
             <label for="exampleInputPassword1">Email</label>
-            <input value={{auth()->user()->email}} type="string" class="form-control" id="exampleInputPassword1" placeholder="email" name="email">
+            <input value="{{auth()->user()->email}}" type="string" class="form-control" id="exampleInputPassword1" placeholder="email" name="email">
             </div>
             <div class="checkout__input">
                 <label for="exampleInputPassword1">Phone Number</label>
-                <input value={{auth()->user()->mobile}} type="string" class="form-control" id="exampleInputPassword1" placeholder="mobile" name="mobile">
+                <input value="{{auth()->user()->mobile}}" type="text" class="form-control" id="exampleInputPassword1" placeholder="mobile" name="mobile">
                 </div>
         <div class="checkout__input">
             <label for="exampleInputPassword1">address</label>
-            <input type="string" class="form-control" id="exampleInputPassword1" placeholder="address" name="address">
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="address" name="address">
         </div>
         
         
